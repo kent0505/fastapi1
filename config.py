8e78dff35b015abc1dfdfccad3b832c8a94c462d
Otabek <otabekyusupov0550@gmail.com>
2023-11-23 23:18:13 +0500

    second commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-name =      "Otabek"
-surname =   "Yusupov"
-password =  "111"                 # admin password
-key =       "hfasdg7SDF6gFGsgdf4" # jwt secret key
-algorithm = "HS256"               # jwt algorithm
-expiry =    24                    # jwt expiry in hours
-domain =     "rechela.uz"         # domain name
+name =      ""
+surname =   ""
+password =  "" # admin password
+key =       "" # jwt secret key
+algorithm = "" # jwt algorithm
+expiry =    24 # jwt expiry in hours
+domain =     "" # domain name
 origins =   [f"https://{domain}", f"https://www.{domain}"]
