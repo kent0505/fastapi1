2d3e794e7fc13dc9ec9b934d48ccc580ccfe256f
Otabek <otabekyusupov0550@gmail.com>
2023-11-29 21:24:31 +0500

    second commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# origins =   ["https://yourdomain.com", "https://www.yourdomain.com"] # allowed hosts
-# url =       "https://yourdomain.com"
-# docs_url =  "/docs" # url for swagger
-# password =  "123" # admin password
-# key =       "fhasd4h3uRhewirASDrwjh34iorSssf45" # jwt secret key
-# algorithm = "HS256" # jwt algorithm
-# expiry =    24 # jwt expiry in hours
+origins =   ["https://yourdomain.com", "https://www.yourdomain.com"] # allowed hosts
+url =       "https://yourdomain.com"
+docs_url =  "/docs" # url for swagger
+password =  "123" # admin password
+key =       "fhasd4h3uRhewirASDrwjh34iorSssf45" # jwt secret key
+algorithm = "HS256" # jwt algorithm
+expiry =    24 # jwt expiry in hours
