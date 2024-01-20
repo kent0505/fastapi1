0ff43dcad09a3dc0b736db091cfdbd3fb1200e52
kent0505 <otabekyusupov0550@gmail.com>
2024-01-21 02:16:39 +0500

    ccc

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
-HOST      = "localhost:8000" # change this to your domain
-URL       = f"http://{HOST}" # change http to https
-KEY       = "" # jwt secret key
-ORIGINS   = [f"https://{HOST}", f"https://www.{HOST}"]
-DOCS_URL  = "/docs" # swagger url
+import logging
+
+HOST     = "localhost:8000" # change this to your domain
+URL      = f"http://{HOST}" # change http to https
+KEY      = ""
+ORIGINS  = [f"https://{HOST}", f"https://www.{HOST}"]
+DOCS_URL = "/docs"
+FILENAME = "logfile.log"
+LEVEL    = logging.INFO
+FORMAT   = "%(asctime)s - %(levelname)s - %(message)s"
+DATEFMT  = "%d-%m-%Y %H:%M:%S"
--- app/main.py
+++ app/main.py
@@ -2,38 +2,23 @@ from fastapi                 import FastAPI, Depends
 from fastapi.staticfiles     import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.auth.jwt_bearer     import JwtBearer
+from app.routers.home        import router as home_router
 from app.routers.user        import router as user_router
 from app.routers.category    import router as category_router
 from app.routers.blog        import router as blog_router
 from app.routers.content     import router as content_router
 from app.routers.image       import router as image_router
 from app.routers.logs        import router as logs_router
-from app.home                import router as home_router
+from app.routers2.product    import router as product_router
 from app.config              import *
 import logging
 import os
 
-
-
 os.makedirs("static", exist_ok=True)
-
-logging.basicConfig(
-    filename="logfile.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%d-%m-%Y %H:%M:%S"
-)
+# logging.basicConfig(filename=FILENAME, level=LEVEL, format=FORMAT, datefmt=DATEFMT)
 
 app = FastAPI(docs_url=DOCS_URL, redoc_url=None)
-
-app.add_middleware(
-    middleware_class=CORSMiddleware,
-    allow_origins=ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
+app.add_middleware(middleware_class=CORSMiddleware, allow_origins=ORIGINS, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.mount(path="/images",    app=StaticFiles(directory="static"),    name="static")
 app.mount(path="/templates", app=StaticFiles(directory="templates"), name="templates")
 
@@ -44,6 +29,7 @@ app.include_router(blog_router,     prefix="/api/v1/blog",     tags=["Blog"],   
 app.include_router(content_router,  prefix="/api/v1/content",  tags=["Content"],  dependencies=[Depends(JwtBearer())])
 app.include_router(image_router,    prefix="/api/v1/upload",   tags=["Image"],    dependencies=[Depends(JwtBearer())])
 app.include_router(logs_router,     prefix="/api/v1/logs",     tags=["Logs"],     dependencies=[Depends(JwtBearer())])
+app.include_router(product_router,  prefix="/api/v1/logs",     tags=["Product"])
 
 
 # app.include_router(category_router, prefix="/api/v1/category", tags=["Category"])
@@ -52,5 +38,7 @@ app.include_router(logs_router,     prefix="/api/v1/logs",     tags=["Logs"],   
 # app.include_router(image_router,    prefix="/api/v1/upload",   tags=["Image"])
 # app.include_router(logs_router,     prefix="/api/v1/logs",     tags=["Logs"])
 # pip install -r requirements.txt
-# cd desktop/backend/fastapi/test2 & source venv/bin/activate
+# cd Desktop/backend/fastapi/test2
+# source venv/bin/activate
 # uvicorn app.main:app --reload
+# sudo lsof -t -i tcp:8000 | xargs kill -9
